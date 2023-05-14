c3547eef86d46f12f3545c126737a5b5597321af
LFRezende <lfsrsprofessional@gmail.com>
2023-05-14 02:31:33 -0300

    Error on big number, deleted deploy.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -10,6 +10,10 @@ const networkConfig = {
   },
 };
 
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 200000000000;
 module.exports = {
   networkConfig,
+  developmentChains,
 };
